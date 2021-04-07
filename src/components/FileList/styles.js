8920d3fe335220602799d238cc1671b6756a30d1
Ddiegoreis <diego.valga13@gmail.com>
2021-04-06 22:58:41 -0300

    a

---
+++ src/components/FileList/styles.js
@@ -0,0 +1,58 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+	margin-top: 20px;
+
+	li {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		color: #444;
+
+		& + li {
+			margin-top: 15px;
+        }
+        
+        div {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        }
+    }
+`
+
+export const FileInfo = styled.div`
+	display: flex;
+	align-items: center;
+
+	div {
+		display: flex;
+        flex-direction: column;
+        
+		span {
+			font-size: 12px;
+			color: #999;
+            margin-top: 5px;
+            align-self: flex-start;
+
+			button {
+				border: 0;
+				background: transparent;
+				color: #e57878;
+				margin-left: 5px;
+				cursor: pointer;
+			}
+		}
+	}
+`
+
+export const Preview = styled.div`
+    width: 36px;
+    height: 36px;
+    border-radius: 5px;
+    background-image: url(${props => props.src});
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center;
+    margin-right: 10px;
+`
